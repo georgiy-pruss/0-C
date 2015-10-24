20abad4c7214361b955bc64cae5c6ac0f65173fe
Georgiy Pruss <yru2l8@yahoo.com>
2015-10-24 16:55:24 +0300

    Create _.h

---
+++ _.h
@@ -0,0 +1,19 @@
+#ifndef _
+
+#define V void
+#define C char
+typedef char* S;
+typedef unsigned char B;
+#define I int
+typedef unsigned int U;
+typedef unsigned long long L;
+#define K const
+#define O static /* own */
+
+#define __ {
+#define _  }
+
+#define R return
+#define DO(i,n) for(I i=0;i<(n);++i)
+
+#endif
