50a2c8998c5441bbbf469d8785e2f43bc8773919
Georgiy Pruss <yru2l8@yahoo.com>
2015-10-24 17:45:50 +0300

    well, too much for macro

--- x.h
+++ x.h
@@ -1,33 +1,45 @@
 /* Simple dynamic string class */
 
-#ifndef X0 /* well, instead of artificial __X__ etc */
+#ifndef X0 /* well, it serves well instead of artificial __X_H__ etc */
 
-#include <_.h>
+#include <string.h> // strlen,memcpy,etc
+#include <stdlib.h> // malloc,free
+#include "_.h"
 
-typedef struct X { union { S p; C s[8]; }; U len; U n; } X;
+typedef struct X { union { S p; C s[8]; }; U l; U n; } X;
 
 #define X0 {0,0,0} /* initializer for X x=X0; for statics done automatically */
 
 // initialization; undefined state -> defined
-#define XI(x)
-#define XIN(x,n)
-#define XIS(x,s)
-#define XIM(x,m,l)
+// -- be sure to use only XI* in undefined state and other ops only in defined!
+#define XI(x)       xi(&(x)) //((x).p=0,(x).n=(x).l=0)
+#define XIN(x,n)    ((x).p=(S)malloc(n+1),(x).n=n,(x).len=0,*p='\0')
+#define XIS(x,s)    (XIM((x),(s),(strlen(s))))
+#define XIM(x,m,k)  ((x).l=(k),((x).l>8)?((x).n=(x).l,(x).p=(S)malloc((x).l+1),memcpy((x).p,(m),(x).l+1)):((x).n=0,memcpy((x).s,(m),(x).l+1)))
+#define XIX(x,x2)   (0)
 // finalization=free; defined state -> undefined
-#define XF(x)
+#define XF(x)       ((x).n>0?(void)free((x).p):(void)0)
 // copy (with underscores - no check if fits)
-#define XCS(x,s)
-#define XCM(x,m,l)
-#define XCS_(x,s)
-#define XCM_(x,m,l)
+#define XCS(x,s)    (0)
+#define XCM(x,m,k)  (0)
+#define XCX(x,x2)   (0)
+#define XCS_(x,s)   (0)
+#define XCM_(x,m,k) (0)
+#define XCX_(x,x2)  (0)
 // append
-#define XAS(x,s)
-#define XAM(x,m,l)
-#define XAS_(x,s)
-#define XAM_(x,m,l)
+#define XAS(x,s)    (0)
+#define XAM(x,m,k)  (0)
+#define XAX(x,x2)   (0)
+#define XAS_(x,s)   (0)
+#define XAM_(x,m,k) (0)
+#define XAX_(x,x2)  (0)
 // truncate
-#define XT(x,l)
+#define XT(x,k)     ((k)>(x).l?((x).l>8?((x).p[k]='\0'):((x).s[k]='\0')):(void)0)
 // reallocate to current size
 #define XR(x)
+// access to data
+#define XP(x) ((x).n>8?(x).p:(x).s)
+#define XL(x) ((x).l)
+#define XN(x) ((x).n)
 
 #endif
