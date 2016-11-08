a144e9648ad144fb2e53c7b98dff382acb73df46
Georgiy Pruss <yru2l8@yahoo.com>
2016-11-08 03:59:04 +0200

    Now this is real program!
    Analog clock, as planned, with ini file.

--- w10clk.c
+++ w10clk.c
@@ -1,55 +1,141 @@
-// compile with: gcc w10clk.c -lgdi32 -lcomdlg32 -Wl,--subsystem,windows -o w10clk.exe
-// (-std=c11 is default)
+// compile with: gcc w10clk.c -lgdi32 -Wl,--subsystem,windows -o w10clk.exe
+// (-std=c11 is default) if needed, -lcomdlg32 or -lcomctl32
 // https://www.cygwin.com/faq.html#faq.programming.win32-no-cygwin
 // http://parallel.vub.ac.be/education/modula2/technology/Win32_tutorial/index.html
 
-// TODO 1. recentering/resizing
-// TODO 2. draw clock hands and ticks
 // TODO 3. round window
-// TODO 4. pop up calendar etc
+// TODO 4. pop up calendar etc --> no, better stopwatch/timer
 // TODO 5. always on top
 // TODO 6. don't show application window in usual taskbar etc
-
-/*
-https://www-user.tu-chemnitz.de/~heha/petzold/ch05d.htm
-SetPixel(hdc, x, y, crColor);
-MoveToEx (hdc, xBeg, yBeg, NULL);
-LineTo (hdc, xEnd, yEnd);
-Rectangle(hdc, 1, 1, 5, 4); // within bounding box
-Ellipse(hdc, xLeft, yTop, xRight, yBottom);
-Arc   (hdc, xLeft, yTop, xRight, yBottom, xStart, yStart, xEnd, yEnd);
-Chord (hdc, xLeft, yTop, xRight, yBottom, xStart, yStart, xEnd, yEnd);
-Pie   (hdc, xLeft, yTop, xRight, yBottom, xStart, yStart, xEnd, yEnd);
-*/
 
 // Trim fat from windows
 #define WIN32_LEAN_AND_MEAN
 #pragma comment(linker, "/subsystem:windows") // for MSVC only
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 #include <windows.h>
-#include <commdlg.h>
 
 // Parameters
-COLORREF g_bgcolor = RGB(196,136,255); // all bg color
-int g_initX=100, g_initY=100;  // initial position
-int g_width=280, g_height=280; // initial size
+COLORREF g_bgcolor = RGB(196,136,255); // bg color
+int g_initX=6, g_initY=6;      // initial position
+int g_width=200, g_height=212; // initial size
+int g_tick_w = 4; COLORREF g_tick_rgb = RGB(64,64,64);
+int g_shand_w = 1; COLORREF g_shand_rgb = RGB(0,0,0);
+int g_mhand_w = 3; COLORREF g_mhand_rgb = RGB(0,0,0);
+int g_hhand_w = 6; COLORREF g_hhand_rgb = RGB(0,0,0);
+BOOL g_seconds = FALSE;
+BOOL g_upd_title = FALSE;
+BOOL g_resizable = FALSE;
+
+void
+read_ini()
+{
+  char fn[500];
+  size_t fnsz = GetModuleFileName(NULL,fn,sizeof(fn));
+  if( fnsz <= 0 || strcmp(fn+fnsz-4,".exe") != 0 ) return;
+  strcpy(fn+fnsz-3,"ini");
+  char s[100] = ""; DWORD rc; int x,y,r,g,b;
+  rc = GetPrivateProfileString( "window", "init", "6,6", s, sizeof(s), fn);
+  if( rc>2 && sscanf( s,"%d,%d", &x,&y )==2 ) { g_initX = x; g_initY = y; }
+  rc = GetPrivateProfileString( "window", "size", "200,212", s, sizeof(s), fn);
+  if( rc>2 && sscanf( s,"%d,%d", &x,&y )==2 ) { g_width = x; g_height = y; }
+  rc = GetPrivateProfileString( "window", "bg", "196,136,255", s, sizeof(s), fn);
+  if( rc>4 && sscanf( s,"%d,%d,%d", &r,&g,&b )==3 ) { g_bgcolor = RGB(r,g,b); }
+  rc = GetPrivateProfileString( "window", "ticks", "4,64,64,64", s, sizeof(s), fn);
+  if( rc>6 && sscanf( s,"%d,%d,%d,%d", &x,&r,&g,&b)==4 ) { g_tick_w = x; g_tick_rgb = RGB(r,g,b); }
+  rc = GetPrivateProfileString( "window", "shand", "1,0,0,0", s, sizeof(s), fn);
+  if( rc>6 && sscanf( s,"%d,%d,%d,%d", &x,&r,&g,&b)==4 ) { g_shand_w = x; g_shand_rgb = RGB(r,g,b); }
+  rc = GetPrivateProfileString( "window", "mhand", "4,0,0,0", s, sizeof(s), fn);
+  if( rc>6 && sscanf( s,"%d,%d,%d,%d", &x,&r,&g,&b)==4 ) { g_mhand_w = x; g_mhand_rgb = RGB(r,g,b); }
+  rc = GetPrivateProfileString( "window", "hhand", "6,0,0,0", s, sizeof(s), fn);
+  if( rc>6 && sscanf( s,"%d,%d,%d,%d", &x,&r,&g,&b)==4 ) { g_hhand_w = x; g_hhand_rgb = RGB(r,g,b); }
+  rc = GetPrivateProfileString( "window", "seconds", "no", s, sizeof(s), fn);
+  if( rc>1 ) g_seconds = strcmp(s,"yes")==0;
+  rc = GetPrivateProfileString( "window", "title", "yes", s, sizeof(s), fn);
+  if( rc>1 ) g_upd_title = strcmp(s,"yes")==0;
+  rc = GetPrivateProfileString( "window", "resizable", "no", s, sizeof(s), fn);
+  if( rc>1 ) g_resizable = strcmp(s,"yes")==0;
+}
 
 // Const and global state vars
 const int ID_TIMER = 1;
-HFONT g_hfFont = 0;
 RECT g_rcClient;
-HBRUSH hbrWhite, hbrGray;
+HBRUSH hbrBG;
+HPEN hsPen, hmPen, hhPen, htPen;
+HINSTANCE g_hinst;
 
 const char*
 calendar()
 {
-  return "Mo Tu We Th Fr Sa Su\n"
-    "      1  2  3  4  5  6\n"
-    " 7  8  9 10 11 12 13\n"
-    "14 15 16 17 18 19 20\n"
-    "21 22 23 24 25 26 27\n"
-    "28 29 30";
+  return "Mo Tu We Th Fr Sa Su\n       1  2  3  4  5  6\n 7  8  9 10 11 12 13\n"
+         "14 15 16 17 18 19 20\n21 22 23 24 25 26 27\n28 29 30";
+}
+
+double sind(double x) { return sin(x*M_PI/180); }
+double cosd(double x) { return cos(x*M_PI/180); }
+
+void
+update_clock(HDC hdc,int halfw, int halfh)
+{
+  time_t t = time(NULL);
+  struct tm* tmptr = localtime(&t);
+  if( !tmptr ) return;
+  int s = tmptr->tm_sec;
+  int m = tmptr->tm_min;
+  int h = tmptr->tm_hour;
+  if( h>12 ) h -= 12;
+
+  double angle_sec = 270.0 + (s*6.0);
+  double angle_min = 270.0 + (m*6.0 + s*0.1);
+  double angle_hour = 270.0 + (h*30 + m*0.5);
+
+  int R = min(halfw,halfh);
+  int mlen = R*83/100;   // Length of minute hand (pixels?) 0.83
+  int secx = (int)( mlen * cosd(angle_sec) );
+  int secy = (int)( mlen * sind(angle_sec) );
+  int minx = (int)( mlen * cosd(angle_min) );
+  int miny = (int)( mlen * sind(angle_min) );
+  int hourx = (int)( 0.72*mlen * cosd(angle_hour) );
+  int houry = (int)( 0.72*mlen * sind(angle_hour) );
+
+  if( g_seconds )
+    SelectObject(hdc, hsPen),
+    MoveToEx(hdc, halfw, halfh, NULL),
+    LineTo(hdc, halfw + secx, halfh + secy);
+  SelectObject(hdc, hmPen);
+  MoveToEx(hdc, halfw, halfh, NULL);
+  LineTo(hdc, halfw + minx, halfh + miny); // lineWidth = 4
+  SelectObject(hdc, hhPen);
+  MoveToEx(hdc, halfw, halfh, NULL);
+  LineTo(hdc, halfw + hourx, halfh + houry);  // lineWidth = 6
+}
+
+void draw_clock(HDC hdc,int halfw, int halfh)
+{
+  SelectObject(hdc, htPen);
+  int R = min(halfw,halfh);
+  //lineWidth = 2;
+  for(int i=0;i<12;++i)
+  {
+    int start_x = (int)( halfw+(0.88*R*cosd(30*i))+0.5 );
+    int start_y = (int)( halfh+(0.88*R*sind(30*i))+0.5 );
+    int end_x   = (int)( halfw+(0.96*R*cosd(30*i))+0.5 );
+    int end_y   = (int)( halfh+(0.96*R*sind(30*i))+0.5 );
+    MoveToEx(hdc,start_x,start_y,NULL);
+    LineTo(hdc,end_x,end_y);
+  }
+}
+
+void
+redraw(HWND hwnd,HDC hDC)
+{
+  if(hwnd) hDC = GetDC(hwnd);
+  SetBkMode(hDC, OPAQUE);
+  FillRect(hDC, &g_rcClient, hbrBG);
+  draw_clock( hDC, g_rcClient.right/2, g_rcClient.bottom/2 );
+  update_clock( hDC, g_rcClient.right/2, g_rcClient.bottom/2 );
+  if(hwnd) ReleaseDC(hwnd, hDC);
 }
 
 // Windows Procedure Event Handler
@@ -60,55 +146,32 @@ WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
   {
     case WM_CREATE: // Window is being created
     {
-      HDC hDC = GetDC(NULL);
-      long lfHeight = -MulDiv(16, GetDeviceCaps(hDC, LOGPIXELSY), 72);
-      ReleaseDC(NULL, hDC);
-      HFONT hf = CreateFont(lfHeight, 0, 0, 0, 0, TRUE, 0, 0, 0, 0, 0, 0, 0,
-          "Times New Roman");
-      if( hf )
-        g_hfFont = hf;
-      else
-        MessageBox(hwnd, "Font creation failed!", "Error", MB_OK | MB_ICONEXCLAMATION);
       if( SetTimer(hwnd, ID_TIMER, 1000, NULL) == 0 )
         MessageBox(hwnd, "Could not SetTimer()!", "Error", MB_OK | MB_ICONEXCLAMATION);
-      hbrWhite = GetStockObject(WHITE_BRUSH);
-      hbrGray  = GetStockObject(GRAY_BRUSH);
+      hbrBG = CreateSolidBrush(g_bgcolor);
+      hsPen = CreatePen(PS_SOLID,g_shand_w,g_shand_rgb);
+      hmPen = CreatePen(PS_SOLID,g_mhand_w,g_mhand_rgb);
+      hhPen = CreatePen(PS_SOLID,g_hhand_w,g_hhand_rgb);
+      htPen = CreatePen(PS_SOLID,g_tick_w,g_tick_rgb);
       return 0;
     }
     case WM_TIMER:
     {
       time_t t = time(NULL);
       struct tm* tmptr = localtime(&t);
-      if( !tmptr )
+      if( !tmptr ) return 0;
+      if( g_upd_title )
       {
-        MessageBox(hwnd, "Could not get localtime!", "Error", MB_OK | MB_ICONEXCLAMATION);
-        return 0;
+        char text[100];
+        strftime( text, sizeof(text), "%T %a %m/%d", tmptr );
+        SetWindowText(hwnd,text);
       }
-      char text[100];
-      size_t sz = strftime( text, sizeof(text), "%T", tmptr );
-      HDC hDC = GetDC(hwnd);
-      TextOut(hDC,50,98,text,sz);
-      strftime( text, sizeof(text), "%T %a %m/%d", tmptr );
-      SetWindowText(hwnd,text);
-      ReleaseDC(hwnd, hDC);
+      redraw(hwnd,0);
       return 0;
     }
-    case WM_SIZE:
+    case WM_SIZE: // will send WM_PAINT as well
     {
       GetClientRect(hwnd, &g_rcClient);
-      char text[100];
-      sprintf( text, "%d %d %d %d",
-          g_rcClient.left, g_rcClient.top, g_rcClient.right, g_rcClient.bottom );
-      HDC hDC = GetDC(hwnd);
-      TextOut(hDC,50,80,text,strlen(text));
-      SetBkMode(hDC,TRANSPARENT);
-      TextOut(hDC,g_rcClient.right/2,g_rcClient.bottom/2,"^",1);
-      int w=g_rcClient.right-g_rcClient.left;
-      int h=g_rcClient.bottom-g_rcClient.top;
-      SetBkMode(hDC,TRANSPARENT);
-      Ellipse(hDC,g_rcClient.right-w/4, g_rcClient.bottom-h/4,
-          g_rcClient.right, g_rcClient.bottom);
-      ReleaseDC(hwnd, hDC);
       return 0;
     }
     case WM_PAINT: // Window needs update
@@ -116,40 +179,10 @@ WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
       SendMessage( hwnd, WM_TIMER, ID_TIMER, 0 ); // to have updated time
       PAINTSTRUCT paintStruct;
       HDC hDC = BeginPaint(hwnd,&paintStruct);
-      HFONT hfOld = g_hfFont ? (HFONT)SelectObject(hDC, g_hfFont) : 0;
-      // Set txt color to blue and background to yellow
-      SetTextColor(hDC, (COLORREF)0x00FF0000); // TTBBGGRR
-      SetBkColor(hDC, (COLORREF)0x0000FFFF);   // = RGB(255,255,0)
-      SetBkMode(hDC, OPAQUE); // or SetBkMode(hDC, TRANSPARENT);
-      // Display text in middle of window
-      const char h_w_msg[] = "Hello, World!"; // Text for display
-      //TextOut(hDC,150,150,h_w_msg,sizeof(h_w_msg)-1);
-      RECT rc = {50,50,170,80};
-      DrawText(hDC, h_w_msg, -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+      redraw(0,hDC);
       EndPaint(hwnd, &paintStruct);
-      if( hfOld ) SelectObject(hDC, hfOld);
       return 0;
     }
-    /*
-    case WM_ERASEBKGND:
-    {
-      HDC hdc = GetDC(hwnd);
-      RECT rc;
-      GetClientRect(hwnd, &rc);
-      SetMapMode(hdc, MM_ANISOTROPIC);
-      SetWindowExtEx(hdc, 100, 100, NULL);
-      SetViewportExtEx(hdc, rc.right, rc.bottom, NULL);
-      FillRect(hdc, &rc, hbrWhite);
-      for( int i = 0; i < 13; ++i )
-      {
-        int x = (i * 40) % 100;
-        int y = ((i * 40) / 100) * 20;
-        SetRect(&rc, x, y, x + 20, y + 20);
-        FillRect(hdc, &rc, hbrGray);
-      }
-      return 1;
-    }
-    */
     case WM_LBUTTONDOWN:
     {
       MessageBox(hwnd, calendar(), "Calendar", MB_OK);
@@ -157,23 +190,14 @@ WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     }
     case WM_RBUTTONDOWN:
     {
-      CHOOSECOLOR cc;
-      static COLORREF acrCustClr[16]; // array of custom colors
-      ZeroMemory(&cc, sizeof(cc));
-      cc.lStructSize = sizeof(cc);
-      cc.hwndOwner = hwnd;
-      cc.lpCustColors = (LPDWORD) acrCustClr;
-      cc.rgbResult = (COLORREF)0;
-      cc.Flags = CC_FULLOPEN | CC_RGBINIT;
-      if( ChooseColor(&cc)==TRUE )
-        cc.rgbResult; // use it
-        // hbrush = CreateSolidBrush(cc.rgbResult);
+      g_seconds = ! g_seconds;
       return 0;
     }
     case WM_CLOSE: // Window is closing
     {
-      if( g_hfFont ) DeleteObject( g_hfFont );
       KillTimer(hwnd, ID_TIMER);
+      DeleteObject(hsPen);
+      DeleteObject(hmPen); DeleteObject(hhPen); DeleteObject(htPen);
       PostQuitMessage(0);
       return 0;
     }
@@ -221,12 +245,15 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdS
   // Register window class
   if( !RegisterClassEx(&windowClass) )
     return 0;
+  g_hinst = hInstance;
+  read_ini();
+  DWORD style = (g_resizable ? WS_OVERLAPPED | WS_SYSMENU | WS_THICKFRAME | WS_VISIBLE
+                             : WS_POPUPWINDOW | WS_BORDER) | WS_CAPTION;
   // Class registerd, so now create window
   HWND hwnd = CreateWindowEx( WS_EX_TOOLWINDOW,    // extended style |WS_EX_TOPMOST
     windowClass.lpszClassName,                     // class name
     "Windows 10 Clock",                            // app name
-    WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_VISIBLE, // window style
-    //WS_POPUPWINDOW | WS_BORDER | WS_CAPTION, // <-- good, but not resizable
+    style,
     g_initX,g_initY, g_width,g_height, // initial position and size
     NULL,         // handle to parent
     NULL,         // handle to menu
