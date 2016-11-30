f0aba28edd01d4370c88a36cd5d5068881029603
georgiy-pruss <yru2l8@yahoo.com>
2016-11-30 03:27:20 +0200

    1.33 - heavy refactoring of proc.c; moon phases

--- W10clk/w10clk_proc.c
+++ W10clk/w10clk_proc.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <wchar.h> // swprintf
 #include <time.h>
 #include <sys/timeb.h>
 #include <unistd.h>
@@ -25,6 +26,7 @@ E V n2ymd( U n, OUT U* y, U* m, U* d );
 E U n2wd( U n );
 E I gettz( I* h, U* m );
 E D moon_phase_date( D myLunation, D phase );
+D t2lunation( D t ) { R floor( (t/(864e2*365.25)+70.0)*12.3685 ); }
 
 C WD[7][4] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 
@@ -40,14 +42,168 @@ fmtj( D j, KS fmt, S s, U mn ) __
   struct tm* tmptr = localtime(&t); ///////tmptr->tm_year -= 2015;
   R strftime( s, mn, fmt, tmptr ); _
 
+K wchar_t*
+moon_phase_name( D x ) __
+  if( 0.00<=x<0.02 ) R L"New Moon  ●";
+  if( 0.02<=x<0.23 ) R L"Waxing Crescent   ☽";
+  if( 0.23<=x<0.27 ) R L"First Quarter   ◐";
+  if( 0.27<=x<0.48 ) R L"Waxing Gibbous ◖";
+  if( 0.48<=x<0.52 ) R L"Full Moon   ◯";
+  if( 0.52<=x<0.73 ) R L"Waning Gibbous   ◗";
+  if( 0.73<=x<0.77 ) R L"Last Quarter   ◑";
+  if( 0.77<=x<0.98 ) R L"Waning Crescent   ☾";
+  if( 0.98<=x<1.01 ) R L"New Moon  ●";
+  R L"Unknown"; _
+
+U
+current_moon( S s, U mn, D t, U n ) __
+  D j = u2j(t);
+  D k = t2lunation(t);
+  D newm = moon_phase_date( k, 0.0 ); // new moon
+  if( j<newm ) { k -= 1.0; newm = moon_phase_date( k, 0.0 ); }
+  D fullm = moon_phase_date( k, 2.0 ); // full moon
+  D nw1m = moon_phase_date( k+1.0, 0.0 ); // next new moon
+  D z = (j-newm)/(nw1m-newm);
+  D v = 0.5 - 0.5*cos( 2*M_PI*z );
+  C f[64]; C m[128];
+  if( j<=fullm ) __ // between new and full
+    fmtj( newm, "Last New Moon: %m/%d %H:%M\n", f, sizeof(f) );
+    U len = sprintf( m, f );
+    fmtj( fullm,"Next Full Moon: %m/%d %H:%M (in %%.1f days)", f, sizeof(f) );
+    sprintf( m+len, f, fullm-j ); _
+  else __ // between full and new
+    fmtj( fullm, "Last Full Moon: %m/%d %H:%M (%%.1f days ago)\n", f, sizeof(f) );
+    U len = sprintf( m, f, j-fullm );
+    fmtj( nw1m, "Next New Moon: %m/%d %H:%M (in %%.1f days)", f, sizeof(f) );
+    sprintf( m+len, f, nw1m-j ); _
+  wchar_t o[512];
+  swprintf( o, 512, L"%S\nPhase now: %.1f days (%.1f%%)\nVisible part: %.1f%%\n%s",
+      moon_phase_name(z), j-newm, 100.0*z, 100.0*v, m );
+  mbw( o, L"W10clk - Moon phase" );
+  R sprintf( s, "phase %.1f%%  visible %.1f%%  full %+.1f", 100.0*z, 100.0*v, fullm-j ); _
+
+U
+show_moon_phases() __
+  // " ● ☽ ◐ ◖  ◯  ◗  ◑  ☾  ● "
+_
+
+U
+current_time_in_many_forms( U c, S s, U mn, time_t t, U n ) __
+  if( c=='j' ) R sprintf( s, "%12.4f", u2j(t) );
+  struct tm* tmptr = localtime(&t);
+  if( c=='u' ) R strftime( s, mn, "%s", tmptr ); // %s -- unix seconds
+  if( c=='d' ) R strftime( s, mn, "%Y/%m/%d %a", tmptr );
+  if( c=='y' ) R strftime( s, mn, "%j", tmptr ); // yday
+  if( c=='t' ) R sprintf( s, "%u:%02u:%02u", tmptr->tm_hour, tmptr->tm_min, tmptr->tm_sec );
+  if( c=='z' ) __
+    if( !tz_is_set ) { tz_offset = gettz( &tz_h, &tz_m ); tz_is_set = true; }
+    R tz_m==0 ? sprintf( s, "%+d", tz_h ) : sprintf( s, "%+d%02u", tz_h, tz_m ); _ R n; _
+
+time_t
+cvt_from_str( S s ) __ U y,m,d, h,mm,ss; struct tm tmv; tmv.tm_isdst = -1;
+  if( sscanf( s, "%u/%u/%u.%u:%u:%u", &y,&m,&d, &h,&mm,&ss )!=6 ) R (time_t)-2;
+  if( !(1970<=y && 1<=m && m<=12 && 1<=d && d<=31 && h<24 && mm<60 && ss<60 &&
+        (y<2106 || y==2106 && (m<2 || m==2 && d<=7))) ) R (time_t)-1;
+  tmv.tm_year = y-1900; tmv.tm_mon = m-1; tmv.tm_mday = d;
+  tmv.tm_hour = h; tmv.tm_min = mm; tmv.tm_sec = ss;
+  R mktime( &tmv ); _
+
+U
+cvt_unix_time( S s, U mn, U n ) __ L u;
+  time_t t = cvt_from_str( s ); if( t == (time_t)-1 ) R n; // 6 nums, but wrong values
+  if( t != (time_t)-2 ) R sprintf( s, "%u", (U)t );        // y/m/d.h:m:s
+  if( sscanf( s, "%llu", &u )!=1 || u>=0x100000000ull ) R n; // limit is 2^32
+  t = (time_t)u; struct tm* tmptr = localtime(&t);
+  R strftime( s, mn, "%Y/%m/%d.%H:%M:%S %a", tmptr ); _
+
+U
+cvt_julian_day( S s, U mn, U n ) __ D j;
+  time_t t = cvt_from_str( s ); if( t == (time_t)-1 ) R n;
+  if( t != (time_t)-2 ) R sprintf( s, "%12.4f", u2j(t) );
+  if( sscanf( s, "%lf", &j )!=1 ) R n;
+  R fmtj( j, "%Y/%m/%d.%H:%M:%S", s, mn ); _
+
+U
+cvt_date( S s, U mn, time_t t, U n ) __ U y,m,d, x; I xd; struct tm* tmptr = localtime(&t);
+  C diff[20] = "";
+  U today = ymd2n( tmptr->tm_year+1900, tmptr->tm_mon+1, tmptr->tm_mday );
+  if( strchr( s, '/' ) ) __ // YYYY/MM/DD or MM/DD
+    I k = sscanf( s, "%u/%u/%u", &y, &m, &d );
+    if( k==3 && 0<y && y<=10000 && 1<=m && m<=12 && 1<=d && d<=31 )
+      x = ymd2n( y, m, d );
+    else if( k==2 && 1<=y && y<=12 && 1<=m && m<=31 ) // y is m, m is d
+      x = ymd2n( tmptr->tm_year+1900, y, m );
+    else R n;
+    if( x != today ) sprintf( diff, " %+d", (I)x - (I)today );
+    R sprintf( s, "%u %s%s", x, WD[n2wd(x)], diff ); _
+  if( s[0]=='+' || s[0]=='-' ) __
+    if( sscanf( s, "%d", &xd )!=1 || -xd>=(I)today ) R n;
+    x = (U)((I)today + xd); _
+  else // NNNNN
+    if( sscanf( s, "%u", &x )!=1 || x==0 ) R n; // don't do it for 0
+  n2ymd( x, &y,&m,&d );
+  if( x != today ) sprintf( diff, " %+d", (I)x - (I)today );
+  R sprintf( s, "%d/%d/%d %s%s", y,m,d, WD[n2wd(x)], diff ); _
+
+U
+cvt_time( S s, U mn, U n ) __ U d,h,mm,ss;
+  KS sign = (s[0]=='-') ? "-" : "";
+  I k = sscanf( ((s[0]=='+' || s[0]=='-') ? s+1 : s), "%u:%u:%u:%u", &d,&h,&mm,&ss );
+  if( k>1 ) __
+    if( k==2 ) { ss=h; mm=d; h=0; d=0; }       //     M:S
+    else if( k==3 ) { ss=mm; mm=h; h=d; d=0; } //   H:M:S
+    I secs = 86400*d + 3600*h + 60*mm + ss;    // D:H:M:S
+    R sprintf( s, "%s%d", sign, secs ); _
+  // else just one number, seconds --> D:H:M:S
+  ss = d%60; d /= 60;
+  mm = d%60; d /= 60;
+  h  = d%24; d /= 24;
+  if( d != 0 ) R sprintf( s, "%s%u:%02u:%02u:%02u", sign, d, h, mm, ss );
+  if( h != 0 ) R sprintf( s, "%s%u:%02u:%02u", sign, h, mm, ss );
+  R sprintf( s, "%s%u:%02u", sign, mm, ss ); _
+
+U
+cvt_yearday( S s, U mn, time_t t, U n ) __ U y,m,d; struct tm* tmptr = localtime(&t);
+  I k = sscanf( s, "%u/%u/%u", &y, &m, &d );
+  if( k==3 ) // Y/M/D
+    R sprintf( s, "%u/%u", y, ymd2n( y,m,d ) - ymd2n( y,1,1 ) + 1 );
+  if( k==2 && (y<=12 && m<=31) ) __ // M/D current year
+    d=m; m=y; y = tmptr->tm_year+1900;
+    R sprintf( s, "%u", ymd2n( y,m,d ) - ymd2n( y,1,1 ) + 1 ); _
+  if( k==2 && (y>12 || m>31) ) __ // y/yday --> y/m/d
+    n2ymd( ymd2n( y,1,1 ) - 1 + m, &y,&m,&d );
+    R sprintf( s, "%u/%u/%u", y, m, d ); _
+  if( k==1 ) __ // yday (in y) --> m/d
+    n2ymd( ymd2n( tmptr->tm_year+1900,1,1 ) - 1 + y, &y,&m,&d );
+    n = sprintf( s, "%u/%u", m, d ); _                           R n; _
+
+U
+cvt_time_zone( S s, U mn, time_t t, U n ) __
+  if( !tz_is_set ) { tz_offset = gettz( &tz_h, &tz_m ); tz_is_set = true; }
+  if( s[0]=='.' ) { mb( g_tzlist+1, "W10clk - time zones" ); R 0; }
+  if( 'A'<=s[0] && s[0]<='Z' ) __ // named time zone
+    S tzn = malloc( n+3 );
+    tzn[0] = ' '; strcpyupr( tzn+1, s ); tzn[n+1] = ' '; tzn[n+2]='\0'; // " TZN "
+    S pos = strstr( g_tzlist, tzn ); I tzv;
+    if( pos && sscanf( pos+n+1, "%d", &tzv )==1 ) n = sprintf( s, "%d", tzv );
+    free( tzn ); _
+  else if( s[0]=='+' || s[0]=='-' || '0'<=s[0] && s[0]<='9' ) __ I z; // number N +N -N
+    if( sscanf( s, "%d", &z )==1 ) __
+      U m = 0; U h = (U)(z>=0 ? z : -z); if( h>=15 ) { m=h%100; h=h/100; }
+      I seconds = 3600*h + 60*m; if( z<0 ) seconds = -seconds;
+      time_t new_t = t - 60*tz_offset + seconds;
+      struct tm* tmptr = localtime(&new_t);
+      n = strftime( s, mn, "%H:%M:%S %a %m/%d", tmptr ); _ _
+  R n; _
+
 U
 process_char( U c, S s, U mn, U n ) __ // mn - max length
-  time_t t; struct tm* tmptr;  U y,m,d, w,h, x; I k,a,z; L xx; D b; // temp. vars.
+  time_t t; D x; L h;
   struct timeb tb;
   O time_t t_start;       O I t_start_ms;
   O I      t_idle;        O I t_idle_ms;
   O time_t t_break_start; O I t_break_start_ms;
-  if( '0'<=c && c<='9' || 'A'<=c && c<='Z' || memchr( "+-*/%^o\\&|!.:()#", c, 16 ) ) __
+  if( '0'<=c && c<='9' || 'A'<=c && c<='Z' || memchr( "+*-/%^o\\&|!.:()#", c, 16 ) ) __
     s[n]=(C)c; if(n<mn) ++n; s[n]=0; _
   else if( c==32 || c==27 ) __ // space escape
     n=0; s[n]=0; _
@@ -55,179 +211,29 @@ process_char( U c, S s, U mn, U n ) __ // mn - max length
     if( n>0 ) --n; s[n]=0; _
   else if( c=='\t' ) __ // tab
     g_seconds = ! g_seconds; _
-  else if( c=='u' || c=='d' || c=='t' || c=='z' || c=='y' ) __
+  else if( c=='u' || c=='d' || c=='t' || c=='z' || c=='y' || c=='j' || c=='m' ) __
     t = time(NULL);
-    tmptr = localtime(&t); // assume it can't be NULL
-    U today = ymd2n( tmptr->tm_year+1900, tmptr->tm_mon+1, tmptr->tm_mday );
-    if( n==0 ) __ // current time in many forms
-      if( c=='u' )
-        n = strftime( s, mn, "%s", tmptr ); // %s -- unix seconds
-      else if( c=='d' )
-        n = strftime( s, mn, "%Y/%m/%d %a", tmptr );
-      else if( c=='y' )
-        n = strftime( s, mn, "%j", tmptr ); // yday
-      else if( c=='z' ) __
-        if( !tz_is_set ) { tz_offset = gettz( &tz_h, &tz_m ); tz_is_set = true; }
-        n = tz_m==0 ? sprintf( s, "%+d", tz_h ) : sprintf( s, "%+d%02u", tz_h, tz_m ); _
-      else // 't'
-        n = sprintf( s, "%u:%02u:%02u", tmptr->tm_hour, tmptr->tm_min, tmptr->tm_sec ); _
-    else __
-      if( c=='u' ) __ // unixtime
-        if( sscanf( s, "%u/%u/%u.%u:%u:%u", &y,&m,&d, &h,&w,&x )==6 ) __ // YYYY/MM/DD.HH:MM:SS
-          if( 1970<=y && 1<=m && m<=12 && 1<=d && d<=31 && h<24 && w<60 && x<60 &&
-              (y<2106 || y==2106 && (m<2 || m==2 && d<=7)) ) __
-            tmptr->tm_year = y-1900; tmptr->tm_mon = m-1; tmptr->tm_mday = d;
-            tmptr->tm_hour = h; tmptr->tm_min = w; tmptr->tm_sec = x;
-            t = mktime( tmptr );
-            if( (I)t != -1 ) n = sprintf( s, "%u", (U)t ); _ _
-        else if( sscanf( s, "%llu", &xx )==1 && xx<0x100000000ull ) __ // NNNNNNN
-          time_t t1 = (time_t)xx;
-          tmptr = localtime(&t1);
-          n = strftime( s, mn, "%Y/%m/%d.%H:%M:%S %a", tmptr ); _ _
-      else if( c=='d' ) __ // NNNNNNd or YYYY/MM/DDd
-        C diff[20] = "";
-        if( strchr( s, '/' ) ) __ // YYYY/MM/DD or MM/DD
-          k = sscanf( s, "%u/%u/%u", &y, &m, &d );
-          if( k==3 && 0<y && y<=10000 && 1<=m && m<=12 && 1<=d && d<=31 )
-            x = ymd2n( y, m, d );
-          else if( k==2 && 1<=y && y<=12 && 1<=m && m<=31 ) // y is m, m is d
-            x = ymd2n( tmptr->tm_year+1900, y, m );
-          else
-            return n; // for now, the only return from inside . . . . . . . . . . . . . . .
-          if( x != today ) sprintf( diff, " %+d", (I)x - (I)today );
-          n = sprintf( s, "%u %s%s", x, WD[n2wd(x)], diff ); _
-        else if( s[0]=='+' || s[0]=='-' ) __ I xd;
-          if( sscanf( s, "%d", &xd )==1 && -xd<(I)today ) __
-            x = (U)((I)today + xd);
-            n2ymd( x, &y,&m,&d );
-            if( x != today ) sprintf( diff, " %+d", (I)x - (I)today );
-            n = sprintf( s, "%d/%d/%d %s%s", y,m,d, WD[n2wd(x)], diff ); _ _
-        else __ // NNNNN
-          if( sscanf( s, "%u", &x )==1 && x>0 ) __ // don't do it for 0
-            n2ymd( x, &y,&m,&d );
-            if( x != today ) sprintf( diff, " %+d", (I)x - (I)today );
-            n = sprintf( s, "%d/%d/%d %s%s", y,m,d, WD[n2wd(x)], diff ); _ _ _
-      else if( c=='t' ) __ // time conversions
-        KS s_abs = (s[0]=='+' || s[0]=='-') ? s+1 : s;
-        KS sign = (s[0]=='-') ? "-" : "";
-        k = sscanf( s_abs, "%u:%u:%u:%u", &d,&h,&m,&x );
-        if( k>1 ) __
-          if( k==2 ) { x=h; m=d; h=0; d=0; }        // M:S
-          else if( k==3 ) { x=m; m=h; h=d; d=0; }   // H:M:S
-          I secs = 86400*d + 3600*h + 60*m + x;   // D:H:M:S
-          n = sprintf( s, "%s%d", sign, secs ); _
-        else if( k==1 ) __ // just one number, seconds --> D:H:M:S
-          x = d%60; d /= 60;
-          m = d%60; d /= 60;
-          h = d%24; d /= 24;
-          if( d != 0 ) n = sprintf( s, "%s%u:%02u:%02u:%02u", sign, d, h, m, x );
-          else if( h != 0 ) n = sprintf( s, "%s%u:%02u:%02u", sign, h, m, x );
-          else n = sprintf( s, "%s%u:%02u", sign, m, x ); _ _
-      else if( c=='y' ) __ // year day -> M/D this year or Y/M/D --> yd
-        k = sscanf( s, "%u/%u/%u", &y, &m, &d );
-        if( k==3 ) __ // Y/M/D
-          n = sprintf( s, "%u/%u", y, ymd2n( y,m,d ) - ymd2n( y,1,1 ) + 1 ); _
-        else if( k==2 && (y<=12 && m<=31) ) __ // M/D current year
-          x = tmptr->tm_year+1900;
-          n = sprintf( s, "%u", ymd2n( x,y,m ) - ymd2n( x,1,1 ) + 1 ); _
-        else if( k==2 && (y>12 || m>31) ) __ // y/yday --> y/m/d
-          n2ymd( ymd2n( y,1,1 ) - 1 + m, &y,&m,&d );
-          n = sprintf( s, "%u/%u/%u", y, m, d ); _
-        else if( k==1 || k==2 && m>31 ) __ // yday (in y) --> m/d
-          n2ymd( ymd2n( tmptr->tm_year+1900,1,1 ) - 1 + y, &y,&m,&d );
-          n = sprintf( s, "%u/%u", m, d ); _ _
-      else if( c=='z' ) __ // timezone
-        if( !tz_is_set ) { tz_offset = gettz( &tz_h, &tz_m ); tz_is_set = true; }
-        if( s[0]=='.' ) mb( g_tzlist+1, "W10clk - time zones" );
-        if( 'A'<=s[0] && s[0]<='Z' ) __
-          S tzn = malloc( n+3 );
-          tzn[0] = ' '; strcpyupr( tzn+1, s ); tzn[n+1] = ' '; tzn[n+2]='\0'; // " TZN "
-          S pos = strstr( g_tzlist, tzn ); I tzv;
-          if( pos && sscanf( pos+n+1, "%d", &tzv )==1 ) n = sprintf( s, "%d", tzv );
-          free( tzn ); _
-        if( s[0]=='+' || s[0]=='-' || '0'<=s[0] && s[0]<='9' ) __ // number N +N -N
-          k = sscanf( s, "%d", &z ); h = (U)(z>=0 ? z : -z);
-          if( k==1 ) __
-            if( h<15 ) m=0; else m=h%100, h=h/100; // split into h,m
-            I seconds = 3600*h + 60*m; if( z<0 ) seconds = -seconds;
-            time_t new_t = t - 60*tz_offset + seconds;
-            tmptr = localtime(&new_t);
-            n = strftime( s, mn, "%H:%M:%S %a %m/%d", tmptr ); _ _ _
-      else // it can't be here
-        n = sprintf( s, "huh?" ); _ _
-  else if( c=='m' ) __ // moon phase
-    if( n==0 ) __
-    /*
-    mbw( L"n ● ☽ ◐ ◖  ◯  ◗  ◑  ☾  ●",L"moon");
-  o = ""
-  if dn<dn2:
-    o += 'New moon:  %02d/%02d %02d:%02d  - %s\n' % (m0,d0,h0,n0,dh(t-t0))
-    o += 'Full moon: %02d/%02d %02d:%02d  + %s\n' % (m2,d2,h2,n2,dh(t2-t))
-  else:
-    o += 'Full moon: %02d/%02d %02d:%02d  - %s\n' % (m2,d2,h2,n2,dh(t-t2))
-    o += 'New moon:  %02d/%02d %02d:%02d  + %s\n' % (m1,d1,h1,n1,dh(t1-t))
-  z = (t-t0)/(t1-t0)
-  o += '\nPhase now: %.1f%%' % (100.0*z)
-  PN = [("New Moon  ●",0.00,0.02),
-        ("Waxing Crescent   ☽",0.02,0.23),
-        ("First Quarter   ◐",0.23,0.27),
-        ("Waxing Gibbous ◖",0.27,0.48),
-        ("Full Moon   ◯",0.48,0.52),
-        ("Waning Gibbous   ◗",0.52,0.73),
-        ("Last Quarter   ◑",0.73,0.77),
-        ("Waning Crescent   ☾",0.77,0.98),
-        ("New Moon  ●",0.98,1.01)]
-  for mn,m1,m2 in PN:
-    if m1<=z<m2:
-      break
-  o += "      %s      " % mn
-  PP = 3.14159265358979*2
-  vi = 0.5 - 0.5*math.cos( PP*z )
-  o += '\nVisible part: %.1f%%' % (100.0*vi)
-    */
-      D t = (D)time(NULL);
-      D j = u2j(t);
-      D k = floor( (t/(864e2*365.25)+70.0)*12.3685 );
-      D newm = moon_phase_date( k, 0.0 ); // new moon
-      if( j<newm ) { k -= 1.0; newm = moon_phase_date( k, 0.0 ); }
-      D fullm = moon_phase_date( k, 2.0 ); // full moon
-      if( j<=fullm ) __ // between new and full
-        n = fmtj( newm, "%m/%d.%H:%M:%S", s, mn ); strcpy( s+n, " new, " ); n += 6;
-        n += fmtj( fullm, "%m/%d.%H:%M:%S", s+n, mn-n ); strcpy( s+n, " full" ); n += 5; _
-      else __ // between full and new
-        newm = moon_phase_date( k+1.0, 0.0 );
-        n = fmtj( fullm, "%m/%d.%H:%M:%S", s, mn ); strcpy( s+n, " full, " ); n += 7;
-        n += fmtj( newm, "%m/%d.%H:%M:%S", s+n, mn-n ); strcpy( s+n, " new" ); n += 4; _
-      _
-    else __
-      //if( sscanf( s, "%lf", &b )==1 ) { b = (b-32.0)/1.8; n = sprintf( s, "%.1f%cC", b, 176 ); }
-      _
-    _
-  else if( c=='j' ) __
-    t = time(NULL);
-    tmptr = localtime(&t); // assume it can't be NULL
     if( n==0 )
-      n = sprintf( s, "%12.4f", u2j(time(NULL)) );
-    else __ // yyyy/mm/dd.hh:mm:ss to jd or back
-      if( sscanf( s, "%u/%u/%u.%u:%u:%u",  &y,&m,&d, &h,&w,&x )==6 ) __
-        if( 1970<=y && 1<=m && m<=12 && 1<=d && d<=31 && h<24 && w<60 && x<60 &&
-            (y<2106 || y==2106 && (m<2 || m==2 && d<=7)) ) __
-          tmptr->tm_year = y-1900; tmptr->tm_mon = m-1; tmptr->tm_mday = d;
-          tmptr->tm_hour = h; tmptr->tm_min = w; tmptr->tm_sec = x;
-          t = mktime( tmptr );
-          if( (I)t != -1 ) n = sprintf( s, "%12.4f", u2j(t) ); _ _
-      else if( sscanf( s, "%lf", &b )==1 ) __ // jd to yyyy/mm/dd.hh:mm:ss
-        n = fmtj( b, "%Y/%m/%d.%H:%M:%S", s, mn ); _ _ _
+      if( c=='m' ) n = current_moon( s, mn, (D)t, n );
+      else         n = current_time_in_many_forms( c, s, mn, t, n );
+    else
+      if( c=='u' )      n = cvt_unix_time( s, mn, n );
+      else if( c=='d' ) n = cvt_date( s, mn, /* today */ t, n ); // NNNNNN or YYYY/MM/DD
+      else if( c=='t' ) n = cvt_time( s, mn, n );
+      else if( c=='y' ) n = cvt_yearday( s, mn, t, n ); // YD -> M/D this year or Y/M/D --> YD
+      else if( c=='z' ) n = cvt_time_zone( s, mn, t, n );
+      else if( c=='m' ) n = show_moon_phases();
+      else n = cvt_julian_day( s, mn, n ); _ // 'j' -- yyyy/mm/dd.hh:mm:ss to jd or back
   else if( c=='f' ) __ // F --> C
-    if( sscanf( s, "%lf", &b )==1 ) { b = (b-32.0)/1.8; n = sprintf( s, "%.1f%cC", b, 176 ); } _
+    if( sscanf( s, "%lf", &x )==1 ) { n = sprintf( s, "%.1f%cC", (x-32.0)/1.8, 176 ); } _
   else if( c=='c' ) __ // C --> F
-    if( sscanf( s, "%lf", &b )==1 ) { b = b*1.8+32.0; n = sprintf( s, "%.1f%cF", b, 176 ); } _
+    if( sscanf( s, "%lf", &x )==1 ) { n = sprintf( s, "%.1f%cF", (x*1.8)+32.0, 176 ); } _
   else if( c=='k' ) __ // Ki --> k
-    if( sscanf( s, "%lf", &b )==1 ) { n = sprintf( s, "%g", b*1.024 ); } _
+    if( sscanf( s, "%lf", &x )==1 ) { n = sprintf( s, "%g", x*1.024 ); } _
   else if( c=='h' ) __ // help | decimal --> XXXX (hexadecimal)
-    if( sscanf( s, "%llu", &xx )==1 ) n = sprintf( s, "#%llX", xx ); _
+    if( sscanf( s, "%llu", &h )==1 ) n = sprintf( s, "#%llX", h ); _
   else if( c=='=' || c==13 ) __ // = or enter -- calculate expression
-    if( calculate( s, &b ) == 0 ) n = sprintf( s, "%.15g", b ); _
+    if( calculate( s, &x ) == 0 ) n = sprintf( s, "%.15g", x ); _
   else if( c=='s' ) __ // start stopwatch
     ftime(&tb); t_start = tb.time; t_start_ms = (I)tb.millitm;
     t_break_start = 0; t_idle = 0; t_idle_ms = 0;
